0ae38572f3d36fb62c9576e8a21f3b722066bf7c
MeruzhanS <meruzhansargsyan48@gmail.com>
2024-06-11 18:37:49 -0700

    screen is up

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 CFLAGS = gcc -Wall -Wextra -std=gnu99
 LIBSRC = lib/raylib/src
+LIBS = -L$(LIBSRC) -lraylib -lm -ldl -framework Cocoa -framework IOKit -framework CoreVideo -framework OpenGL
 
 default: main
 
 main: src/main.c
-	$(CFLAGS) -I$(LIBSRC) -o bin/$@ $<
+	$(CFLAGS) -I$(LIBSRC) -o bin/$@ $< $(LIBS)
+
+run: bin/main
+	$<
--- src/main.c
+++ src/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
-#include "raylib.h"
+#include <raylib.h>
+
+#define HEIGHT 800
+#define WIDTH 800
 
 int main(void) {
-    printf("Hello World\n");
+    InitWindow(WIDTH, HEIGHT, "Hello World");
+    SetTargetFPS(30);
+
+    while(!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
 
