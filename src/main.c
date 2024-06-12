54a349c80e940ac7a050ddc236b1589b09326404
MeruzhanS <meruzhansargsyan48@gmail.com>
2024-06-11 22:47:59 -0700

    drawing done, picture not so

--- src/main.c
+++ src/main.c
@@ -4,13 +4,24 @@
 #define HEIGHT 800
 #define WIDTH 800
 
+#define BOXSZ 500
+
 int main(void) {
+    int printed = 0;
     InitWindow(WIDTH, HEIGHT, "Hello World");
     SetTargetFPS(30);
 
     while(!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        {
+            ClearBackground(RAYWHITE);
+            DrawRectangle((WIDTH - BOXSZ)/2, (HEIGHT - BOXSZ)/2, BOXSZ, BOXSZ, BLACK);
+            Image img = LoadImage("GraphGeneratorImg.png");
+            if(IsImageReady(img) && !printed)  {
+                printf("Image loaded\n");
+                printed = 1;
+            }
+        }
         EndDrawing();
     }
     CloseWindow();
